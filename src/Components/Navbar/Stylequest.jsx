23c68ef23a17859cbcb4d40a439c1d3b3c9e0d90
gudia18 <sgudia879@gmail.com>
2024-07-15 03:09:54 +0530

    add stylequest

--- src/Components/Navbar/Stylequest.jsx
+++ src/Components/Navbar/Stylequest.jsx
@@ -1,72 +1,60 @@
-// Stylequest.jsx
-import React from "react";
-
 export const Stylequest = () => {
-    return (
-        <div
-            className="ddown"
-            style={{
-                marginLeft: "-350px",
-            }}
-        >
-            <div className="flex">
-                <div className="a1 b">
-                    <ul>
-                        <li>Casual Wear</li>
-                        <li>T-Shirts</li>
-                        <li>Jeans</li>
-                        <li>Shorts</li>
-                        <li>Jackets</li>
-                    </ul>
-                </div>
-                <div className="a2 b">
-                    <ul>
-                        <li>Formal Wear</li>
-                        <li>Shirts</li>
-                        <li>Trousers</li>
-                        <li>Blazers</li>
-                        <li>Suits</li>
-                    </ul>
-                    <ul className="nextul">
-                        <li>Accessories</li>
-                        <li>Watches</li>
-                        <li>Bags</li>
-                        <li>Sunglasses</li>
-                    </ul>
-                </div>
-                <div className="a1 b">
-                    <ul>
-                        <li>Shoes</li>
-                        <li>Casual Shoes</li>
-                        <li>Formal Shoes</li>
-                        <li>Sports Shoes</li>
-                        <li>Boots</li>
-                    </ul>
-                    <ul className="nextul">
-                        <li>Grooming</li>
-                        <li>Skincare</li>
-                        <li>Haircare</li>
-                        <li>Beard Care</li>
-                    </ul>
-                </div>
-                <div className="a2 b">
-                    <ul>
-                        <li>Sports & Activewear</li>
-                        <li>T-Shirts</li>
-                        <li>Shorts</li>
-                        <li>Tracksuits</li>
-                        <li>Sneakers</li>
-                    </ul>
-                    <ul className="nextul">
-                        <li>Top Brands</li>
-                        <li>Nike</li>
-                        <li>Adidas</li>
-                        <li>Puma</li>
-                        <li>Reebok</li>
-                        <li>Levi's</li>
-                    </ul>
-                </div>
+   return (
+      <div className="ddown">
+      
+      <a href="/men">
+         <div className="flex">
+            <div className="a1 m">
+               <ul>
+                  <li>Virtual Try On</li>
+                  <li>AR/VR </li>
+                  <li>3D  Visualization</li>
+                  <li>Body Shapes and Skin Tones</li>
+                  <li>Personalization</li>
+                  <li>Social Sharing</li>
+               </ul>
+               <ul className="nextul m">
+                  <li>Lingerie Match</li>
+                  <li>Fit and Style</li>
+                  <li>Color Matching</li>
+               </ul>
             </div>
-        </div>
-    );
+            <div className="a2 m">
+               <ul>
+                  <li>Community Forum</li>
+                  <li>Topics</li>
+                  <li>Virtual Events</li>
+                  <li>Local Meetups</li>
+                  <li>Brand Ambassadors</li>
+                  <li>User Contributions</li>
+               </ul>
+
+               <ul className="nextul m">
+                  <li>Contests</li>
+                  <li>Challenges</li>
+                  <li>Themes</li>
+                  <li>Prizes</li>
+                  <li>LeaderBoard</li>
+                  <li>Voting System</li>
+               </ul>
+               <ul className="nextul m">
+                  <li>Rewards</li>
+               </ul>
+            </div>
+            <div className="a1 m">
+               <ul>
+                  <li>SpendRush</li>
+                  <li>Flash Sales</li>
+                  <li>Member-Only Offers</li>
+                  <li>Bundle Deals</li>
+               </ul>
+               
+            </div>
+           
+            
+         </div>
+      </a>
+         
+      </div>
+   );
 };
